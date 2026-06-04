fcd3f08d6835b889b79922223258711af42f0c5a
Fikri Andra Irham <fikri.andrhm@gmail.com>
2026-06-04 22:59:49 +0700

    feat: implement Prisma repository layer and user task relations

---
+++ src/routes/users.routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+const { getTasksByUser } = require('../controllers/tasks.controller');
+
+/**
+ * @swagger
+ * /users/{userId}/tasks:
+ *   get:
+ *     summary: Ambil semua task milik user tertentu
+ *     tags: [Users]
+ *     parameters:
+ *       - in: path
+ *         name: userId
+ *         required: true
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Berhasil
+ *       404:
+ *         description: User tidak ditemukan
+ */
+router.get('/:userId/tasks', getTasksByUser);
+
+module.exports = router;
